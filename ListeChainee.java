42eff30513f2c72b2ecd40257e246c77cb4fed98
DIDIer5454 <95764559+DIDIer5454@users.noreply.github.com>
2022-01-11 11:37:43 +0100

    js

---
+++ ListeChainee.java
@@ -0,0 +1,105 @@
+public class ListeChainee implements Liste{
+
+    private int tete;
+    private  MaillonEntier[] tab;
+
+    public ListeChainee(int p){
+        this.tab=new MaillonEntier[p];
+        for (int i=0;i<this.tab.length;i++){
+            this.tab[i]= new MaillonEntier(null,-2);
+        }
+        this.tete=-1;
+        }
+
+    public SousTitre val(int p){
+       return (this.tab[p].getVal());
+    }
+    public void suplis(int p)
+    {
+        if(p==this.tete)
+        {
+            int suc = this.tab[this.tete].getSuc();
+            this.LibererPlace(this.tete);
+            this.tete = suc;
+        }
+        else
+        {
+            if(this.tab[p].getSuc()!=-1)
+            {
+                int i = this.tete;
+                while(this.suc(i) != p)
+                {
+                    i++;
+                }
+                int suc = this.tab[p].getSuc();
+                this.tab[i].setSuc(suc);
+                this.LibererPlace(p);
+            }
+            else
+            {
+                int i = 0;
+                while(this.suc(i) !=p)
+                {
+                    i++;
+                }
+                this.tab[i].setSuc(-1);
+                this.LibererPlace(p);
+            }
+        }
+    }
+
+
+    public void adjtlis(SousTitre s){
+int i =retournerPlaceLibre();
+int h = this.tete;
+this.tete=i;
+this.tab[i].setSuc(h);
+this.tab[i].setVal(s);
+
+        
+    }
+
+    public void adjlis(int p, SousTitre s){
+        int i =retournerPlaceLibre();
+       int h = this.tab[p].getSuc();
+        this.tab[p].setSuc(i);
+        this.tab[i].setVal(s);
+        this.tab[i].setSuc(h);
+
+    }
+
+    public boolean finliste(int p){
+        return(p==-1);
+    }
+
+    public int tete(){
+       return(this.tete);
+    }
+
+    public int suc(int p){
+       int i=this.tab[p].getSuc();
+       return i;
+    }
+
+    public int retournerPlaceLibre(){
+        boolean find = false ;
+        int j = 0;
+        while (find == false) {
+            for (int i = 0; i < this.tab.length; i++) {
+                if (this.tab[i].getSuc() == -2) {
+                    j=i;
+                    find=true;
+
+                }
+            }
+        }
+        return j;
+    }
+
+
+    public void LibererPlace(int p)
+    {
+        this.tab[p].setSuc(-2);
+        this.tab[p].setVal(null);
+    }
+}
--- ListeTriee.java
+++ ListeTriee.java
@@ -2,12 +2,12 @@ public class ListeTriee{
 
 	// Attribut de liste sous-jacente
 	private Liste liste;
-	
+
 	public ListeTriee(Liste listevide){
 		// Affectation de la liste vide a l'attribut prive
 		liste = listevide;
 	}
-	
+
 	/**
 	 * ajoute un element au bon endroit dans la liste triee
 	 * @param chaine element a inserer
@@ -15,15 +15,15 @@ public class ListeTriee{
 	public void adjlisT(String chaine){
 		throw (new Error("not implemented"));
 	}
-	
+
 	/**
 	 * permet de supprimer un element d'une liste. Supprime le premier element dont la valeur est egale a "chaine" ; ne fait rien si "chaine" n'appartient pas a la liste.
-	 * @param chaine l'element a supprimer 
+	 * @param chaine l'element a supprimer
 	 */
 	public void suplisT(String chaine){
 		throw (new Error("not implemented"));
 	}
-	
+
 	/**
 	 * Retourne vrai si au moins un element de la liste a une valeur egale a "chaine", et retourne faux sinon.
 	 * @param chaine l'element que l'on recherche
